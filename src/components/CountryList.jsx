218bc208386a60bbf293c7ab2e9046b38115968b
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2025-07-18 16:59:39 +0100

    implemented the context API functionality and complete the city UI

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,8 +2,10 @@ import Spinner from "./Spinner";
 import styles from "./CountryList.module.css";
 import Message from "./Message";
 import CountryItem from "./CountryItem";
+import { useCities } from "../contexts/CitiesContexts";
 
-export default function CountryList({ cities, isLoading }) {
+export default function CountryList() {
+  const {cities, isLoading} = useCities()
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
